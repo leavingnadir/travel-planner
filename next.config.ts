c180430194da2012fe7d36ce44e22fbde6a7afbe
Hirusha Ranaweera <leavingnadir.inc@gmail.com>
2025-11-07 02:43:19 +0530

    Configure dashboard

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "ovco9b5jyh.ufs.sh",
+      },
+      {
+        hostname: "3b5gjm0ir4.ufs.sh",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
